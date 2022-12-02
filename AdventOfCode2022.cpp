e31dd8eefa388ddb9e9b852e4c9c1417b52c7d49
Animalien <Animalien@users.noreply.github.com>
2022-12-02 11:00:07 -0800

    Solved Problem 1

--- AdventOfCode2022.cpp
+++ AdventOfCode2022.cpp
@@ -526,7 +526,85 @@ private:
 ////////////////////////////
 // Problem 1 - Calorie Counting
 
+void AbsorbCalorieCountingTotal(BigInt newTotal, BigIntList& biggestTotalsList)
+{
+    if (newTotal < biggestTotalsList[biggestTotalsList.size() - 1])
+        return;
 
+    auto iter = biggestTotalsList.begin();
+    while ((iter != biggestTotalsList.end()) && (newTotal < *iter))
+        ++iter;
+
+    biggestTotalsList.insert(iter, newTotal);
+    biggestTotalsList.pop_back();
+}
+
+void PrintCalorieCountingBiggestTotalsList(const BigIntList& biggestTotalsList)
+{
+    BigInt totalTotal = 0;
+    printf("Biggest %lld totals:  ", (BigInt)biggestTotalsList.size());
+    for (BigInt currTotal: biggestTotalsList)
+    {
+        printf("%lld ", currTotal);
+        totalTotal += currTotal;
+    }
+    printf(" (total %lld)\n", totalTotal);
+}
+
+void RunCalorieCountingOnLines(const StringList& lines)
+{
+    BigIntList biggestTotalsList = { 0, 0, 0 };
+
+    BigInt currentTotal = 0;
+
+    for (const std::string& line: lines)
+    {
+        if (line.empty())
+        {
+            printf("Found elf with total %lld\n", currentTotal);
+            AbsorbCalorieCountingTotal(currentTotal, biggestTotalsList);
+            PrintCalorieCountingBiggestTotalsList(biggestTotalsList);
+            printf("\n");
+
+            currentTotal = 0;
+        }
+        else
+        {
+            currentTotal += atoll(line.c_str());
+        }
+    }
+
+    printf("Found elf with total %lld\n", currentTotal);
+    AbsorbCalorieCountingTotal(currentTotal, biggestTotalsList);
+    PrintCalorieCountingBiggestTotalsList(biggestTotalsList);
+    printf("\n");
+}
+
+void RunCalorieCountingTest(std::initializer_list<const char*> list)
+{
+    StringList lines;
+    for (const char* oneLine: list)
+    {
+        lines.push_back(oneLine);
+    }
+    RunCalorieCountingOnLines(lines);
+}
+
+void RunCalorieCountingInputFile()
+{
+    StringList lines;
+    ReadFileLines("Day1Input.txt", lines);
+    RunCalorieCountingOnLines(lines);
+}
+
+void RunCalorieCounting()
+{
+    printf("Test:\n\n");
+    RunCalorieCountingTest({ "1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000" });
+
+    printf("\n\nInput:\n\n");
+    RunCalorieCountingInputFile();
+}
 
 
 ////////////////////////////
@@ -549,6 +627,7 @@ int main(int argc, char** argv)
     switch (problemNum)
     {
         case 1:
+            RunCalorieCounting();
             break;
         default:
             printf("'%s' is not a valid problem number!\n\n", problemArg);

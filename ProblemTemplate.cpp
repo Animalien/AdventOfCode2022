143213b627177957733759003e74ae4c5c62f5d0
Animalien <Animalien@users.noreply.github.com>
2022-12-02 11:55:13 -0800

    Submitted a little problem template for silliness

---
+++ ProblemTemplate.cpp
@@ -0,0 +1,16 @@
+///////////////////////////
+// Problem # - Name
+
+#include "AdventOfCode2022.h"
+
+class Problem# : public ProblemBase
+{
+public:
+	virtual int GetProblemNum() const override { return #; }
+
+	virtual void Run() override
+	{
+	}
+};
+
+Problem# problem#;

6f18ceef39104b10106906823030585422d03694
Animalien <Animalien@users.noreply.github.com>
2022-12-05 11:07:22 -0800

    A little more template

--- ProblemTemplate.cpp
+++ ProblemTemplate.cpp
@@ -6,11 +6,21 @@
 class Problem# : public ProblemBase
 {
 public:
-	virtual int GetProblemNum() const override { return #; }
+    virtual int GetProblemNum() const override { return #; }
 
-	virtual void Run() override
-	{
-	}
+    virtual void Run() override
+    {
+        RunOnData("Day#Example.txt", true);
+        //RunOnData("Day#Input.txt", false);
+    }
+
+    void RunOnData(const char* filename, bool verbose)
+    {
+        printf("For file '%s'...\n", filename);
+
+        StringList lines;
+        ReadFileLines(filename, lines);
+    }
 };
 
 Problem# problem#;

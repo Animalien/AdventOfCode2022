2c4453f71b57d9f6172185253f099fe0cf4a5694
Animalien <Animalien@users.noreply.github.com>
2023-01-01 12:11:00 -0800

    Another attempt at simpler solutions to both parts of Problem 16
    Does not quite work for part 2 yet, will have to revisit

--- Problem16.cpp
+++ Problem16.cpp
@@ -10,6 +10,7 @@ public:
 
     virtual void Run() override
     {
+        //TestPermutationIterator(15);
         RunOnData("Day16Example.txt", true);
         RunOnData("Day16Input.txt", false);
     }
@@ -33,6 +34,81 @@ private:
     BigIntList m_significantNodeIndexList;
     BigIntListList m_signifToSignifDistGrid;
 
+    class IndexIterator
+    {
+    public:
+        IndexIterator(BigInt firstIndex, BigInt lastIndex)
+            : m_firstIndex(firstIndex), m_lastIndex(lastIndex), m_fullPathLength(lastIndex - firstIndex + 1)
+        {
+            assert(m_firstIndex >= 0);
+            assert(m_lastIndex >= m_firstIndex);
+            m_visitedList.resize(m_lastIndex + 1, false);
+            m_currPath.reserve(m_fullPathLength);
+            m_currPath.push_back(m_firstIndex - 1);
+        }
+
+        BigInt GetCurrCandidateIndexForThisStep() const { return m_currPath.back(); }
+        bool GetIndexForPrevStep(BigInt& index) const
+        {
+            if (m_currPath.size() <= 1)
+                return false;
+
+            index = m_currPath[m_currPath.size() - 2];
+
+            return true;
+        }
+
+        bool FindNextCandidateIndexForThisStep()
+        {
+            BigInt& index = m_currPath.back();
+            if (index > m_lastIndex)
+                return false;
+
+            for (;;)
+            {
+                ++index;
+                if (index > m_lastIndex)
+                    return false;
+                if (!m_visitedList[index])
+                    break;
+            }
+
+            return true;
+        }
+
+        bool ProgressToNextStep()
+        {
+            if ((BigInt)m_currPath.size() >= m_fullPathLength)
+                return false;
+
+            m_visitedList[m_currPath.back()] = true;
+            m_currPath.push_back(m_firstIndex - 1);
+
+            return true;
+        }
+
+        bool BackUpOneStep()
+        {
+            if ((BigInt)m_currPath.size() <= 1)
+                return false;
+
+            m_currPath.pop_back();
+            m_visitedList[m_currPath.back()] = false;
+
+            return true;
+        }
+
+        const BigIntList& GetCurrPath() const { return m_currPath; }
+
+    private:
+        BigInt m_firstIndex = 0;
+        BigInt m_lastIndex = 0;
+        BigInt m_fullPathLength = 0;
+
+        BoolList m_visitedList;
+        BigIntList m_currPath;
+    };
+
     void RunOnData(const char* filename, bool verbose)
     {
         printf("For file '%s'...\n", filename);
@@ -45,8 +121,11 @@ private:
 
         BuildSignifToSignifDistGrid(verbose);
 
-        const BigInt mostPressure = CalcMostPressureCanRelease();
-        printf("The most pressure that can be released = %lld\n\n", mostPressure);
+        const BigInt mostPressure = CalcMostPressureCanRelease(30, 1, true);
+        printf("The most pressure that can be released in part one = %lld\n", mostPressure);
+
+        const BigInt mostPressurePartTwo = CalcMostPressureCanRelease(26, 2, verbose);
+        printf("The most pressure that can be released in part two = %lld\n\n", mostPressurePartTwo);
     }
 
     void BuildNodeList(const StringList& lines, BigInt& startIndex, bool verbose)
@@ -236,8 +315,263 @@ private:
 
     static BigInt CalcCharHash(char ch1, char ch2) { return (((BigInt)ch1) << 8) | (BigInt)ch2; }
 
-    BigInt CalcMostPressureCanRelease() const
+    BigInt CalcMostPressureCanRelease(BigInt minutesAvailable, BigInt numSearchers, bool verbose) const
     {
+#if 1
+
+        struct Searcher
+        {
+            Searcher(BigInt minutesAvailable) : minutesRemaining(minutesAvailable) {}
+
+            BigInt currSigIndex = 0;
+            BigInt minutesRemaining = 0;
+        };
+
+        struct Step
+        {
+            BigInt searcherIndex = -1;
+            BigInt fromSigIndex = 0;
+            BigInt toSigIndex = 0;
+            BigInt timeTaken = 0;
+            BigInt pressureContributed = 0;
+        };
+
+        std::vector<Searcher> searcherList;
+        searcherList.resize(numSearchers, Searcher(minutesAvailable));
+
+        std::vector<Step> stepStack;
+        stepStack.reserve(m_significantNodeIndexList.size());
+
+        BigInt mostPressure = 0;
+        std::vector<Step> bestPath;
+
+        BigInt currPressure = 0;
+        IndexIterator ii(1, m_significantNodeIndexList.size() - 1);
+
+        for (;;)
+        {
+            bool allDone = false;
+            for (;;)
+            {
+                if (ii.FindNextCandidateIndexForThisStep())
+                    break;
+
+                if (!ii.BackUpOneStep())
+                {
+                    allDone = true;
+                    break;
+                }
+
+                const Step& stepTop = stepStack.back();
+                Searcher& restoreSearcher = searcherList[stepTop.searcherIndex];
+                restoreSearcher.currSigIndex = stepTop.fromSigIndex;
+                restoreSearcher.minutesRemaining += stepTop.timeTaken;
+
+                currPressure -= stepTop.pressureContributed;
+
+                stepStack.pop_back();
+            }
+
+            if (allDone)
+                break;
+
+            const BigInt nextSignifIndex = ii.GetCurrCandidateIndexForThisStep();
+
+            // find the searcher who is going to take the next step
+            BigInt mostMinutesRemaining = -1;
+            BigInt chosenSearcherIndex = -1;
+            BigInt chosenTimeTaken = -1;
+            BigInt chosenNextMinutesRemaining = -1;
+            for (BigInt s = 0; s < numSearchers; ++s)
+            {
+                Searcher& thisSearcher = searcherList[s];
+                const BigInt currSignifIndex = thisSearcher.currSigIndex;
+                const BigInt dist = m_signifToSignifDistGrid[currSignifIndex][nextSignifIndex];
+                const BigInt timeToOpenValve = dist + 1;
+
+                const BigInt nextMinutesRemaining = thisSearcher.minutesRemaining - timeToOpenValve;
+                if ((nextMinutesRemaining >= 0)
+                    && ((mostMinutesRemaining < 0) || (thisSearcher.minutesRemaining > mostMinutesRemaining)))
+                {
+                    mostMinutesRemaining = thisSearcher.minutesRemaining;
+                    chosenSearcherIndex = s;
+                    chosenTimeTaken = timeToOpenValve;
+                    chosenNextMinutesRemaining = nextMinutesRemaining;
+                }
+            }
+            
+            if (chosenSearcherIndex < 0)
+                continue;
+
+            Searcher& searcher = searcherList[chosenSearcherIndex];
+
+            const BigInt pressureToContribute =
+                chosenNextMinutesRemaining * m_nodeList[m_significantNodeIndexList[nextSignifIndex]].flowRate;
+
+            Step newStep;
+            newStep.searcherIndex = chosenSearcherIndex;
+            newStep.fromSigIndex = searcher.currSigIndex;
+            newStep.toSigIndex = nextSignifIndex;
+            newStep.timeTaken = chosenTimeTaken;
+            newStep.pressureContributed = pressureToContribute;
+            stepStack.push_back(newStep);
+
+            searcher.minutesRemaining = chosenNextMinutesRemaining;
+            searcher.currSigIndex = nextSignifIndex;
+            currPressure += pressureToContribute;
+
+            if (currPressure > mostPressure)
+            {
+                mostPressure = currPressure;
+                bestPath = stepStack;
+            }
+
+            if (!ii.ProgressToNextStep())
+            {
+                currPressure -= pressureToContribute;
+                searcher.currSigIndex = stepStack.back().fromSigIndex;
+                searcher.minutesRemaining += chosenTimeTaken;
+                stepStack.pop_back();
+                continue;
+            }
+        }
+
+        //if (verbose)
+        {
+            printf("Best path:\n");
+            BigInt totalPressure = 0;
+            for (const Step& step: bestPath)
+            {
+                totalPressure += step.pressureContributed;
+                printf(
+                    "  Searcher %lld went from %lld to %lld, taking time %lld, contributing pressure %lld to total pressure %lld\n",
+                    step.searcherIndex,
+                    step.fromSigIndex,
+                    step.toSigIndex,
+                    step.timeTaken,
+                    step.pressureContributed,
+                    totalPressure);
+            }
+            printf("\n");
+        }
+
+        return mostPressure;
+        
+
+#elif 1
+
+        BigInt mostPressure = 0;
+        BigIntList bestPath;
+
+        IndexIterator ii(1, m_significantNodeIndexList.size() - 1);
+
+        BigInt currPressure = 0;
+        BigIntList currPressureStack;
+        BigInt minutesRemaining = minutesAvailable;
+        BigIntList minutesRemainingStack;
+
+        for (;;)
+        {
+            bool allDone = false;
+            for (;;)
+            {
+                if (ii.FindNextCandidateIndexForThisStep())
+                    break;
+
+                if (!ii.BackUpOneStep())
+                {
+                    allDone = true;
+                    break;
+                }
+
+                currPressure = currPressureStack.back();
+                currPressureStack.pop_back();
+
+                minutesRemaining = minutesRemainingStack.back();
+                minutesRemainingStack.pop_back();
+            }
+
+            if (allDone)
+                break;
+
+            BigInt currSignifIndex = 0;
+            ii.GetIndexForPrevStep(currSignifIndex);
+            const BigInt nextSignifIndex = ii.GetCurrCandidateIndexForThisStep();
+            const BigInt dist = m_signifToSignifDistGrid[currSignifIndex][nextSignifIndex];
+            const BigInt timeToOpenNextValve = dist + 1;
+            if (timeToOpenNextValve > minutesRemaining)
+                continue;
+
+            minutesRemaining -= timeToOpenNextValve;
+            const BigInt pressureToContribute =
+                minutesRemaining * m_nodeList[m_significantNodeIndexList[nextSignifIndex]].flowRate;
+            currPressure += pressureToContribute;
+
+            if (currPressure > mostPressure)
+            {
+                mostPressure = currPressure;
+                bestPath = ii.GetCurrPath();
+            }
+
+            if (!ii.ProgressToNextStep())
+            {
+                currPressure -= pressureToContribute;
+                minutesRemaining += timeToOpenNextValve;
+                continue;
+            }
+
+            currPressureStack.push_back(currPressure);
+            minutesRemainingStack.push_back(minutesRemaining);
+        }
+
+        if (verbose)
+        {
+            BigInt total = 0;
+            PrintBestPath("My", minutesAvailable, bestPath, total);
+        }
+
+        return mostPressure;
+
+
+#elif 1
+        BigIntListPermutationIterator pathIter(1, m_significantNodeIndexList.size() - 1);
+
+        BigInt mostTotalPressure = 0;
+        //bestPath.clear();
+
+        while (pathIter.HaveMorePermutations())
+        {
+            BigInt currTotalPressure = 0;
+            BigInt minutesRemaining = minutesAvailable;
+
+            const BigIntList& currPath = pathIter.GetCurrentPermutation();
+            BigInt currSignifIndex = 0;
+            for (BigInt nextSignifIndex: currPath)
+            {
+                const BigInt dist = m_signifToSignifDistGrid[currSignifIndex][nextSignifIndex];
+                const BigInt timeToOpenNextValve = dist + 1;
+                if (timeToOpenNextValve > minutesRemaining)
+                    break;
+
+                minutesRemaining -= timeToOpenNextValve;
+                currTotalPressure += minutesRemaining * m_nodeList[m_significantNodeIndexList[nextSignifIndex]].flowRate;
+
+                currSignifIndex = nextSignifIndex;
+            }
+
+            if (currTotalPressure > mostTotalPressure)
+            {
+                mostTotalPressure = currTotalPressure;
+                //bestPath = currPath;
+            }
+
+            pathIter.Step();
+        }
+
+        return mostTotalPressure;
+
+#else
+
         BoolList visitedList;
         visitedList.resize(m_significantNodeIndexList.size(), false);
 
@@ -245,6 +579,8 @@ private:
         RecursiveCalcPressureCanRelease(visitedList, 0, 30, 0, mostTotalPressure);
 
         return mostTotalPressure;
+
+#endif
     }
 
     void RecursiveCalcPressureCanRelease(
@@ -277,14 +613,189 @@ private:
 
             if (nextMinutesRemaining > 0)
                 RecursiveCalcPressureCanRelease(
-                    visitedList,
-                    nextSignifIndex,
-                    nextMinutesRemaining,
-                    nextValveTotalPressure,
-                    mostTotalPressure);
+                    visitedList, nextSignifIndex, nextMinutesRemaining, nextValveTotalPressure, mostTotalPressure);
         }
 
         visitedList[currSignifIndex] = false;
+    }
+
+    BigInt CalcMostPressureCanReleasePartTwo(bool verbose) const
+    {
+        BoolList visitedList;
+        visitedList.resize(m_significantNodeIndexList.size(), false);
+
+        BigInt mostTotalPressure = 0;
+        bool nextNodeFound = false;
+        BigIntList myPath;
+        myPath.push_back(0);
+        BigIntList elephPath;
+        elephPath.push_back(0);
+        BigIntList myBestPath;
+        BigIntList elephBestPath;
+        RecursiveCalcPressureCanReleasePartTwo(
+            visitedList, 0, 0, 26, 26, 0, mostTotalPressure, nextNodeFound, myPath, elephPath, myBestPath, elephBestPath);
+
+        if (verbose)
+        {
+            BigInt total = 0;
+            PrintBestPath("My", 26, myBestPath, total);
+            PrintBestPath("Elephant's", 26, elephBestPath, total);
+        }
+
+        return mostTotalPressure;
+    }
+
+    void RecursiveCalcPressureCanReleasePartTwo(
+        BoolList& visitedList,
+        BigInt myCurrSignifIndex,
+        BigInt elephCurrSignifIndex,
+        BigInt myMinutesRemaining,
+        BigInt elephMinutesRemaining,
+        BigInt currTotalPressure,
+        BigInt& mostTotalPressure,
+        bool& nextNodeFound,
+        BigIntList& myPath,
+        BigIntList& elephPath,
+        BigIntList& myBestPath,
+        BigIntList& elephBestPath) const
+    {
+        nextNodeFound = false;
+
+        for (BigInt nextSignifIndex = 1; nextSignifIndex < (BigInt)m_significantNodeIndexList.size(); ++nextSignifIndex)
+        {
+            if (visitedList[nextSignifIndex])
+                continue;
+
+            const BigInt nextNodeFlowRate = m_nodeList[m_significantNodeIndexList[nextSignifIndex]].flowRate;
+
+            BigInt iCanReleaseThisMuchPressure = 0;
+            BigInt iWillNeedThisMuchTime = 0;
+            if (nextSignifIndex != myCurrSignifIndex)
+            {
+                const BigInt dist = m_signifToSignifDistGrid[myCurrSignifIndex][nextSignifIndex];
+                iWillNeedThisMuchTime = dist + 1;
+                if (iWillNeedThisMuchTime <= myMinutesRemaining)
+                    iCanReleaseThisMuchPressure = myMinutesRemaining * nextNodeFlowRate;
+            }
+
+            BigInt elephCanReleaseThisMuchPressure = 0;
+            BigInt elephWillNeedThisMuchTime = 0;
+            if (nextSignifIndex != elephCurrSignifIndex)
+            {
+                const BigInt dist = m_signifToSignifDistGrid[elephCurrSignifIndex][nextSignifIndex];
+                elephWillNeedThisMuchTime = dist + 1;
+                if (elephWillNeedThisMuchTime <= elephMinutesRemaining)
+                    elephCanReleaseThisMuchPressure = elephMinutesRemaining * nextNodeFlowRate;
+            }
+
+            if ((iCanReleaseThisMuchPressure <= 0) && (elephCanReleaseThisMuchPressure <= 0))
+                continue;
+
+            if (iCanReleaseThisMuchPressure > 0)
+            {
+                const BigInt myNextSignifIndex = nextSignifIndex;
+                myPath.push_back(myNextSignifIndex);
+
+                const BigInt myNextMinutesRemaining = myMinutesRemaining - iWillNeedThisMuchTime;
+
+                const BigInt nextValveTotalPressure = currTotalPressure + iCanReleaseThisMuchPressure;
+                if (nextValveTotalPressure > mostTotalPressure)
+                {
+                    mostTotalPressure = nextValveTotalPressure;
+                    myBestPath = myPath;
+                    elephBestPath = elephPath;
+                }
+
+                visitedList[nextSignifIndex] = true;
+
+                nextNodeFound = true;
+                bool nextNextNodeFound = false;
+                RecursiveCalcPressureCanReleasePartTwo(
+                    visitedList,
+                    myNextSignifIndex,
+                    elephCurrSignifIndex,
+                    myNextMinutesRemaining,
+                    elephMinutesRemaining,
+                    nextValveTotalPressure,
+                    mostTotalPressure,
+                    nextNextNodeFound,
+                    myPath,
+                    elephPath,
+                    myBestPath,
+                    elephBestPath);
+
+                myPath.pop_back();
+
+                visitedList[nextSignifIndex] = false;
+            }
+
+            if (elephCanReleaseThisMuchPressure > 0)
+            {
+                const BigInt elephNextSignifIndex = nextSignifIndex;
+                elephPath.push_back(elephNextSignifIndex);
+
+                const BigInt elephNextMinutesRemaining = elephMinutesRemaining - elephWillNeedThisMuchTime;
+
+                const BigInt nextValveTotalPressure = currTotalPressure + elephCanReleaseThisMuchPressure;
+                if (nextValveTotalPressure > mostTotalPressure)
+                {
+                    mostTotalPressure = nextValveTotalPressure;
+                    myBestPath = myPath;
+                    elephBestPath = elephPath;
+                }
+
+                visitedList[nextSignifIndex] = true;
+
+                nextNodeFound = true;
+                bool nextNextNodeFound = false;
+                RecursiveCalcPressureCanReleasePartTwo(
+                    visitedList,
+                    myCurrSignifIndex,
+                    elephNextSignifIndex,
+                    myMinutesRemaining,
+                    elephNextMinutesRemaining,
+                    nextValveTotalPressure,
+                    mostTotalPressure,
+                    nextNextNodeFound,
+                    myPath,
+                    elephPath,
+                    myBestPath,
+                    elephBestPath);
+
+                elephPath.pop_back();
+
+                visitedList[nextSignifIndex] = false;
+            }
+        }
+    }
+
+    void PrintBestPath(const char* who, BigInt minutesAvailable, const BigIntList& path, BigInt& total) const
+    {
+        printf("%s best path:\n  0\n", who);
+
+        BigInt prevSigNodeIndex = 0;
+        //-1;
+        for (BigInt sigNodeIndex: path)
+        {
+            const BigInt dist = m_signifToSignifDistGrid[prevSigNodeIndex][sigNodeIndex];
+            const BigInt timeNeeded = dist + 1;
+            minutesAvailable -= timeNeeded;
+            const BigInt contributes = minutesAvailable * m_nodeList[m_significantNodeIndexList[sigNodeIndex]].flowRate;
+            total += contributes;
+
+            printf(
+                "  %lld (d = %lld, tn = %lld, fr = %lld, mr = %lld, c = %lld, tp = %lld)\n",
+                sigNodeIndex,
+                dist,
+                timeNeeded,
+                m_nodeList[m_significantNodeIndexList[sigNodeIndex]].flowRate,
+                minutesAvailable,
+                contributes,
+                total);
+
+            prevSigNodeIndex = sigNodeIndex;
+        }
+        printf("Total = %lld\n\n", total);
     }
 };
 
